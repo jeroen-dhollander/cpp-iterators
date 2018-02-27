043de7b227a999cd2e289a3f6db41f26f1f81663
Jeroen Dhollander <jeroen.dhollander@gmail.com>
2018-02-26 19:57:52 -0800

    Update tests to be more accurate
    I noticed that some of the 'non-const-iterator' tests could pass
    even if we only support a const iterator.
    
    So I updated them to actually modify the values in the collection.

--- iterator/iterator_utilities_test.cc
+++ iterator/iterator_utilities_test.cc
@@ -1,12 +1,14 @@
 
 #include "iterator_utilities.h"
 #include <list>
+#include <memory>
 #include <vector>
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace iterator {
 using std::list;
+using std::string;
 using std::vector;
 using testing::ElementsAre;
 using testing::ElementsAreArray;
@@ -19,83 +21,95 @@ list<int> AnyCollectionReversed() {
   return {5, 3, 1};
 }
 
+// Executes '++' on each value in the iterable.
+// Used to ensure we can non-const access the elements
 template <typename _Iterable>
-auto GetConstValues(const _Iterable& iterable) -> std::list<typename _Iterable::value_type> {
-  std::list<typename _Iterable::value_type> values{};
-  for (const auto& value : iterable)
-    values.push_back(value);
-  return values;
+auto IncreaseAll(_Iterable* iterable) {
+  for (auto& value : *iterable)
+    value++;
+  return *iterable;
 }
 
+// Doubles each value
 template <typename _Iterable>
-auto GetNonConstValues(_Iterable* iterable) -> std::list<typename _Iterable::value_type> {
-  std::list<typename _Iterable::value_type> values{};
+auto DoubleAll(_Iterable* iterable) {
   for (auto& value : *iterable)
-    values.push_back(value);
-  return values;
+    value *= 2;
+  return *iterable;
+}
+
+// Appends the given string to each (string) value in the iterable.
+// Used to ensure we can non-const access the elements
+template <typename _Iterable>
+auto AppendAll(_Iterable* iterable, string suffix) {
+  list<std::string> result;
+  for (auto& value : *iterable) {
+    value += suffix;
+    result.push_back(value);
+  }
+  return result;
 }
 
 TEST(IterateTest, can_iterate_const_collection) {
   list<int> collection{AnyCollection()};
 
-  auto const_iterator = Iterate(std::as_const(collection));
-  EXPECT_THAT(GetConstValues(const_iterator), ElementsAreArray(collection));
+  auto result = Iterate(std::as_const(collection));
+  EXPECT_THAT(std::as_const(result), ElementsAreArray(collection));
 }
 
 TEST(IterateTest, can_const_iterate_non_const_collection) {
   list<int> collection{AnyCollection()};
 
-  auto iterator = Iterate(collection);
-  EXPECT_THAT(GetConstValues(iterator), ElementsAreArray(collection));
+  auto result = Iterate(collection);
+  EXPECT_THAT(std::as_const(result), ElementsAreArray(collection));
 }
 
 TEST(IterateTest, can_non_const_iterate_non_const_collection) {
   list<int> collection{AnyCollection()};
 
   auto iterator = Iterate(collection);
-  EXPECT_THAT(GetNonConstValues(&iterator), ElementsAreArray(collection));
+  EXPECT_THAT(IncreaseAll(&iterator), ElementsAreArray(IncreaseAll(&collection)));
 }
 
 TEST(IterateTest, can_const_iterate_rvalue_collection) {
-  auto iterator = Iterate(AnyCollection());
-  EXPECT_THAT(GetConstValues(iterator), ElementsAreArray(AnyCollection()));
+  auto result = Iterate(AnyCollection());
+  EXPECT_THAT(std::as_const(result), ElementsAreArray(AnyCollection()));
 }
 
 TEST(IterateTest, can_non_const_iterate_rvalue_collection) {
-  auto iterator = Iterate(AnyCollection());
-  EXPECT_THAT(GetNonConstValues(&iterator), ElementsAreArray(AnyCollection()));
+  auto result = Iterate(AnyCollection());
+  EXPECT_THAT(IncreaseAll(&result), ElementsAreArray(IncreaseAll(&AnyCollection())));
 }
 
 TEST(ReverseTest, can_iterate_const_collection) {
   list<int> collection{AnyCollection()};
 
   auto const_iterator = Reverse(std::as_const(collection));
-  EXPECT_THAT(GetConstValues(const_iterator), ElementsAreArray(AnyCollectionReversed()));
+  EXPECT_THAT(std::as_const(const_iterator), ElementsAreArray(AnyCollectionReversed()));
 }
 
 TEST(ReverseTest, can_const_iterate_non_const_collection) {
   list<int> collection{AnyCollection()};
 
-  auto iterator = Reverse(collection);
-  EXPECT_THAT(GetConstValues(iterator), ElementsAreArray(AnyCollectionReversed()));
+  auto result = Reverse(collection);
+  EXPECT_THAT(std::as_const(result), ElementsAreArray(AnyCollectionReversed()));
 }
 
 TEST(ReverseTest, can_non_const_iterate_non_const_collection) {
   list<int> collection{AnyCollection()};
 
-  auto iterator = Reverse(collection);
-  EXPECT_THAT(GetNonConstValues(&iterator), ElementsAreArray(AnyCollectionReversed()));
+  auto result = Reverse(collection);
+  EXPECT_THAT(IncreaseAll(&result), ElementsAreArray(IncreaseAll(&AnyCollectionReversed())));
 }
 
 TEST(ReverseTest, can_const_iterate_rvalue_collection) {
-  auto iterator = Reverse(AnyCollection());
-
-  EXPECT_THAT(GetConstValues(iterator), ElementsAreArray(AnyCollectionReversed()));
+  auto result = Reverse(AnyCollection());
+  EXPECT_THAT(std::as_const(result), ElementsAreArray(AnyCollectionReversed()));
 }
 
 TEST(ReverseTest, can_non_const_iterate_rvalue_collection) {
-  auto iterator = Reverse(AnyCollection());
-  EXPECT_THAT(GetNonConstValues(&iterator), ElementsAreArray(AnyCollectionReversed()));
+  auto result = Reverse(AnyCollection());
+  EXPECT_THAT(IncreaseAll(&result), ElementsAreArray(IncreaseAll(&AnyCollectionReversed())));
 }
 
 TEST(JoinTest, can_iterate_2_const_collections) {
@@ -106,140 +120,134 @@ TEST(JoinTest, can_iterate_2_const_collections) {
   const auto& const_second = second;
 
   auto const_iterator = Join(const_first, const_second);
-  EXPECT_THAT(GetConstValues(const_iterator), ElementsAre(1, 2, 3, 4, 5, 6));
+  EXPECT_THAT(std::as_const(const_iterator), ElementsAre(1, 2, 3, 4, 5, 6));
 }
 
 TEST(JoinTest, can_const_iterate_2_non_const_collection) {
   list<int> first{1, 2, 3};
   vector<int> second{4, 5, 6};
 
-  auto iterator = Join(first, second);
-  EXPECT_THAT(GetConstValues(iterator), ElementsAre(1, 2, 3, 4, 5, 6));
+  auto result = Join(first, second);
+  EXPECT_THAT(std::as_const(result), ElementsAre(1, 2, 3, 4, 5, 6));
 }
 
 TEST(JoinTest, can_non_const_iterate_2_non_const_collection) {
   list<int> first{1, 2, 3};
   vector<int> second{4, 5, 6};
 
-  auto iterator = Join(first, second);
-
-  EXPECT_THAT(GetNonConstValues(&iterator), ElementsAre(1, 2, 3, 4, 5, 6));
+  auto result = Join(first, second);
+  EXPECT_THAT(IncreaseAll(&result), ElementsAre(2, 3, 4, 5, 6, 7));
 }
 
 TEST(JoinTest, can_const_iterate_2_rvalue_collection) {
-  auto iterator = Join(list<int>{1, 2, 3}, vector<int>{4, 5, 6});
-  EXPECT_THAT(GetConstValues(iterator), ElementsAre(1, 2, 3, 4, 5, 6));
+  auto result = Join(list<int>{1, 2, 3}, vector<int>{4, 5, 6});
+  EXPECT_THAT(std::as_const(result), ElementsAre(1, 2, 3, 4, 5, 6));
 }
 
 TEST(JoinTest, can_non_const_iterate_2_rvalue_collection) {
-  auto iterator = Join(list<int>{1, 2, 3}, vector<int>{4, 5, 6});
-  EXPECT_THAT(GetNonConstValues(&iterator), ElementsAre(1, 2, 3, 4, 5, 6));
+  auto result = Join(list<int>{1, 2, 3}, vector<int>{4, 5, 6});
+  EXPECT_THAT(IncreaseAll(&result), ElementsAre(2, 3, 4, 5, 6, 7));
 }
 
 TEST(MapTest, can_const_map_const_collection) {
   std::list<int> input{1, 3, 5};
 
   auto map = Map(std::as_const(input), [](const auto& value) { return std::to_string(value); });
-  EXPECT_THAT(GetConstValues(map), ElementsAre("1", "3", "5"));
-}
-
-TEST(MapTest, can_non_const_map_const_collection) {
-  // Note it might seem weird we can non-const iterate over a const-collection,
-  // but in reality we're not iterating over the collection,
-  // we're actually iterating over the return values of the mapping-function
-  std::list<int> input{1, 3, 5};
-
-  auto map = Map(std::as_const(input), [](const auto& value) { return std::to_string(value); });
-  EXPECT_THAT(GetNonConstValues(&map), ElementsAre("1", "3", "5"));
+  EXPECT_THAT(std::as_const(map), ElementsAre("1", "3", "5"));
 }
 
 TEST(MapTest, can_const_map_non_const_collection) {
   std::list<int> input{1, 3, 5};
 
   auto map = Map(input, [](const auto& value) { return std::to_string(value); });
-  EXPECT_THAT(GetConstValues(map), ElementsAre("1", "3", "5"));
+  EXPECT_THAT(std::as_const(map), ElementsAre("1", "3", "5"));
 }
 
 TEST(MapTest, can_non_const_map_non_const_collection) {
   std::list<int> input{1, 3, 5};
 
   auto map = Map(input, [](const auto& value) { return std::to_string(value); });
-  EXPECT_THAT(GetNonConstValues(&map), ElementsAre("1", "3", "5"));
+  EXPECT_THAT(AppendAll(&map, "-suffix"), ElementsAre("1-suffix", "3-suffix", "5-suffix"));
 }
 
 TEST(MapTest, can_const_map_rvalue_collection) {
   std::list<int> input{1, 3, 5};
 
   auto map = Map(std::move(input), [](const auto& value) { return std::to_string(value); });
-  EXPECT_THAT(GetConstValues(map), ElementsAre("1", "3", "5"));
+  EXPECT_THAT(std::as_const(map), ElementsAre("1", "3", "5"));
 }
 
 TEST(MapTest, can_non_const_map_rvalue_collection) {
   std::list<int> input{1, 3, 5};
 
   auto map = Map(std::move(input), [](const auto& value) { return std::to_string(value); });
-  EXPECT_THAT(GetNonConstValues(&map), ElementsAre("1", "3", "5"));
+  EXPECT_THAT(AppendAll(&map, "-suffix"), ElementsAre("1-suffix", "3-suffix", "5-suffix"));
 }
 
 TEST(MapTest, MapKeys__extract_keys_from_std_map) {
-  std::map<std::string, int> input{{{"a", 1}, {"b", 2}}};
+  std::map<string, int> input{{{"a", 1}, {"b", 2}}};
 
-  EXPECT_THAT(GetConstValues(MapKeys(input)), ElementsAre("a", "b"));
+  EXPECT_THAT(MapKeys(input), ElementsAre("a", "b"));
 }
 
 TEST(MapTest, MapValues__extract_values_from_std_map) {
-  std::map<std::string, int> input{{{"a", 1}, {"b", 2}}};
+  std::map<string, int> input{{{"a", 1}, {"b", 2}}};
 
-  EXPECT_THAT(GetConstValues(MapValues(input)), ElementsAre(1, 2));
+  EXPECT_THAT(MapValues(input), ElementsAre(1, 2));
 }
 
 TEST(FilterTest, can_filter_first_value) {
   std::list<int> input{1, 2};
   auto filter_function = [](const int& value) { return value > 1; };
 
-  EXPECT_THAT(GetConstValues(Filter(input, filter_function)), ElementsAre(2));
+  EXPECT_THAT(Filter(input, filter_function), ElementsAre(2));
 }
 
 TEST(FilterTest, can_filter_consecutive_values) {
   std::list<int> input{1, 2, 3, 4, 5};
   auto filter_function = [](const int& value) { return value == 1 || value == 5; };
 
-  EXPECT_THAT(GetConstValues(Filter(input, filter_function)), ElementsAre(1, 5));
+  EXPECT_THAT(Filter(input, filter_function), ElementsAre(1, 5));
 }
 
 TEST(FilterTest, can_const_filter_const_collection) {
   std::list<int> input{1, 2, 3, 4, 5};
   auto filter_function = [](const int& value) { return value % 2 == 0; };
 
-  EXPECT_THAT(GetConstValues(Filter(std::as_const(input), filter_function)), ElementsAre(2, 4));
+  auto result = Filter(std::as_const(input), filter_function);
+  EXPECT_THAT(std::as_const(result), ElementsAre(2, 4));
 }
 
 TEST(FilterTest, can_const_filter_non_const_collection) {
   std::list<int> input{1, 2, 3, 4, 5};
   auto filter_function = [](const int& value) { return value % 2 == 0; };
 
-  EXPECT_THAT(GetConstValues(Filter(input, filter_function)), ElementsAre(2, 4));
+  auto result = Filter(input, filter_function);
+  EXPECT_THAT(std::as_const(result), ElementsAre(2, 4));
 }
 
 TEST(FilterTest, can_non_const_filter_non_const_collection) {
   std::list<int> input{1, 2, 3, 4, 5};
   auto filter_function = [](const int& value) { return value % 2 == 0; };
 
-  EXPECT_THAT(GetNonConstValues(&Filter(input, filter_function)), ElementsAre(2, 4));
+  auto result = Filter(input, filter_function);
+  EXPECT_THAT(DoubleAll(&result), ElementsAre(4, 8));
 }
 
 TEST(FilterTest, can_const_filter_rvalue_collection) {
   std::list<int> input{1, 2, 3, 4, 5};
   auto filter_function = [](const int& value) { return value % 2 == 0; };
 
-  EXPECT_THAT(GetConstValues(Filter(std::move(input), filter_function)), ElementsAre(2, 4));
+  auto result = Filter(std::move(input), filter_function);
+  EXPECT_THAT(std::as_const(result), ElementsAre(2, 4));
 }
 
 TEST(FilterTest, can_non_const_filter_rvalue_collection) {
   std::list<int> input{1, 2, 3, 4, 5};
   auto filter_function = [](const int& value) { return value % 2 == 0; };
 
-  EXPECT_THAT(GetNonConstValues(&Filter(std::move(input), filter_function)), ElementsAre(2, 4));
+  auto result = Filter(std::move(input), filter_function);
+  EXPECT_THAT(DoubleAll(&result), ElementsAre(4, 8));
 }
 
 }  // namespace iterator
